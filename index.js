2fbe0614343024b41725c721a7bc384cf0990e3b
Abednego <abedikroos15@gmail.com>
2023-03-15 19:47:22 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31;
+const num2 = 2;
+const num3 = 24;
+const num4 = 5;
+const maxSet = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random() * 100);
+
+const mod = num3 % num4;
+
+const max = Math.max(...maxSet);
